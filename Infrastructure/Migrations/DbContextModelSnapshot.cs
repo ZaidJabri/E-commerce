9d3c786871e7472956b80e4045857bb75222e15c
ZaidJabri <zaidjabri1@hotmail.com>
2023-12-29 19:06:00 +0300

    first commit

--- Infrastructure/Migrations/DbContextModelSnapshot.cs
+++ Infrastructure/Migrations/DbContextModelSnapshot.cs
@@ -15,7 +15,6 @@ namespace Infrastructure.Migrations
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
-#pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "8.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
